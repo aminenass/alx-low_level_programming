021dc3dbb52ef37f6244e2a71781ccfe4b1475e0
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-15 06:26:59 -0700

    fforget the comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void)
+int main(void0)
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
