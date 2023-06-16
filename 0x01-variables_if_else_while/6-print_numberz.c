62b91fe23fe7382d8342bc01dc3a478c9c15583f
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-16 09:29:23 -0700

    print loop 0 9 withiutprint f

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints thenmb without char.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num)
+	{
+		putchar(num +'0');
+	}
+	putchar("\n");
+	return (0);
+}
