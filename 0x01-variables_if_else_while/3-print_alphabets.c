421a083bf874f2b98b81f363ec507b6456978414
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-16 08:39:27 -0700

    alphaetloweerupper case fixage2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@ int main(void)
 	char letter_lo = 'a';
 	char letter_up = 'A';
 
-	while (letter_lo <= 'z')	
+	while (letter_lo <= 'z')
 	{
 		putchar(letter_lo);
 		letter_lo++;
 	}
 	while (letter_up <= 'Z')
-        {
+	{
 		putchar(letter_up);
 		letter_up++;
 	}
