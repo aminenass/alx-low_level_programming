e3bfeda47ca15053fbf32cc471d11d8b1cec3574
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-16 08:53:25 -0700

    alphaet print except q &&e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main print alphapet except q&e
+ *
+ * return always success 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter<='z')
+	{
+		if (letter!='q' && letter!='e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
+
