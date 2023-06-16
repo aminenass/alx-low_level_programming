e3ada7455ad5e9cec2c5808b3169c6a5e55bd837
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-16 12:02:18 -0700

    putchar number with ,fixage 3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-        int number;
+	int number;
 
-        for (number = 0; number < 10; number++)
-        {
+	for (number = 0; number < 10; number++)
+	{
 		putchar(number + '0');
 		if (number != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
