91927e674946003ed61ac463b1f83cf34dbe1bb9
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 07:01:41 -0700

    print alphabet 10x time fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - This is the preferred style for multi-line
- * comments in C source code.
+ * print_alphabet_x10 - This is a function print alphabet 10x
+ * 
  * Return - success 0
  */
 void print_alphabet_x10(void)
 {
 	char alpha= 'a';
-	n = 0;
+	int n = 0;
 
 	while (n <= 10 && alpha <= 'z')
 	{
