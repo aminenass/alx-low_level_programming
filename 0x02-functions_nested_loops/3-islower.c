ea6028661ec92b17967c4cf7ff86026a247b4668
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 07:26:52 -0700

    print alphabet 10x time fixage by doing loopinsideloop 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - This code check if lower case or not
+* if lower is 1 else 0.
+*/
+int _islower(int c)
+{
+	if ( 'a'<= c <'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
