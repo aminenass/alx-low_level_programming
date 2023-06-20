048f7af2de4baeabf4f71bb35132d59f65fbbc9f
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 07:10:14 -0700

    print alphabet 10x time fixage by doing loopinsideloo^p

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char alpha = 'a';
 	int n = 0;
 
-	while (n <= 10 && alpha <= 'z')
+	while ( n<=10 )
 	{
-		_putchar(alpha);
-		n++ , alpha++ ;
-		
+		char alpha = 'a';
+
+		while ( alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++ ;
+		}
+		_putchar('\n');
+		n++ ;
 	}
-	_putchar('\n');
 }
