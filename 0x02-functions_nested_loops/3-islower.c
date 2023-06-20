a6d688e747f9da087f93fa2f258b2f4e423162c4
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 07:32:52 -0700

    check if alphabet lower or not fix1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
 * _islower - This code check if lower case or not
-* if lower is 1 else 0.
-* int c - represent the alpha to check
-* Return : 0 always success
+* @c: represent the alpha to check
+* Return: 1 is lower and otherwise 0
 */
 int _islower(int c)
 {
