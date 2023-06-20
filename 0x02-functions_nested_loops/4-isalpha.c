3d7140447232f234d08e0fa515215d21b7273b58
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 07:41:44 -0700

    check if alphabet lower or not fix2

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#inculde "main.h"
+/**
+* _isalpha - This is a func that check if alphabet
+* @c: the parameter which represent the chari
+* Return: if alphabet either lower || upper r 1 otherwise 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
+
