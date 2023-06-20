736ff7ffe54dccd4adaad06905654e6454d7b81c
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 08:44:35 -0700

    print value of n fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
 * _abs : This is a funtion print value
+* @n : represent the parameter num
 * Return : return n or -n depend either pos or neg
 */
-int _abs(int)
+int _abs(int n)
 {
 	if (n >= 0 )
 	{
