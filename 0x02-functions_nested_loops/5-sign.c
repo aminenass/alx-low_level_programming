86f943bfd2466dd0a130c99b64bcb8ab683ee538
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 08:33:00 -0700

    check signal n return 1 0 or -1 fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * int print_sign - this function prints the sign of a number.
+ * print_sign - this function prints the sign of a number.
  * @n : the paramether as signal
- * Return : 1 and + if n >0 otherwise -1 - or 0 if n ==0
+ * Return: 1 and + if n >0 otherwise -1 - or 0 if n ==0
  */
 int print_sign(int n)
 {
@@ -16,9 +16,9 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
 	}
-}	 
+}
