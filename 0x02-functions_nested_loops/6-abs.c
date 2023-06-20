8f60b014654428a2d788464461ea91cc1ad23bf6
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 08:46:47 -0700

    print value of n fixage

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* _abs : This is a funtion print value
+* _abs - This is a funtion print value
 * @n : represent the parameter num
 * Return : return n or -n depend either pos or neg
 */
 int _abs(int n)
 {
-	if (n >= 0 )
+	if (n >= 0)
 	{
 		return (n);
 	}
 	return (-n);
-}	
+}
