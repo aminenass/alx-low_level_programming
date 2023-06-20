ce7e52a47f1bb3167ed8ef45de6a90e228f4708e
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 08:42:20 -0700

    print value of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs : This is a funtion print value
+* Return : return n or -n depend either pos or neg
+*/
+int _abs(int)
+{
+	if (n >= 0 )
+	{
+		return (n);
+	}
+	return (-n);
+}	
