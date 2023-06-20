df8e377e992b7ae98c155f4b9f4d051651487b1f
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 07:03:31 -0700

    print alphabet 10x time fixage2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - This is a function print alphabet 10x
- * 
+ *
  * Return - success 0
  */
 void print_alphabet_x10(void)
 {
-	char alpha= 'a';
+	char alpha = 'a';
 	int n = 0;
 
 	while (n <= 10 && alpha <= 'z')
 	{
 		_putchar(alpha);
-		n++;
+		n++ , alpha++ ;
+		
 	}
 	_putchar('\n');
 }
