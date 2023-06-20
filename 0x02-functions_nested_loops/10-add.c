7ac7dc4dd2f28daa2c165ae0201d75c828f9aaee
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 09:38:30 -0700

    addition of two int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int add - this funct add two int into re
+ * @a: represent first int
+ * @b: second int
+ * Return: equal to c addition of a and b
+ */
+int add(int a, int b)
+{
+	c = a + b;
+	return (c);
+}
