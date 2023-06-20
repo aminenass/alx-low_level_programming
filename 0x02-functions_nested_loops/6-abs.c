0d041c8932215562d674bf980e0cf113ef2a5079
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 08:48:22 -0700

    print value of n fixage 1

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
 * _abs - This is a funtion print value
 * @n : represent the parameter num
-* Return : return n or -n depend either pos or neg
+* Return: return n or -n depend either pos or neg
 */
 int _abs(int n)
 {
