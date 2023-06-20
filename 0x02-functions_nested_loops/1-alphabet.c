d64e6c976019fb5f9d70890bc8768ab8845c009a
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-20 06:18:36 -0700

    print alphabet lowercase _putchar fixage6

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  * main - print_alphabet from func .
- *  Description:  A lower case alphabet is printed,
+ * Description:  A lower case alphabet is printed,
  * Return: Always 0 (Success)
+ * This function prints all the lowercase alphabets from 'a' to 'z'.
  */
+
 void print_alphabet(void)
 {
 	char alpha = 'a';
