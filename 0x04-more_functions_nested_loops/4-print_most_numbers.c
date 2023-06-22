e96233dd37bf8f3fe836d65d042b59a9941bca16
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-22 08:12:21 -0700

    princt num except 2 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers(void) - function print number except
+ * Return: return number except 4 2
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
+
+
