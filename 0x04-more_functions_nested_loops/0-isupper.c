1376b8184523ba5a458d9b2edd5d5d802f4855fc
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-22 03:04:37 -0700

    check upper case && main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* _isupper - this function checck if digit
+ *@c: the digit
+ * Return: if digit return 1 otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}	
