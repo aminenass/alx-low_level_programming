8dbbc7b840b1fed6a4c70480d03109e24cfcac52
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-22 08:26:48 -0700

    princt num except 2 4 fixage spelling mistake

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_most_number - function print number except
+ * print_most_numbers - function print number except
  * Return: return number except 4 2
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 		{
