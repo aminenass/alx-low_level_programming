aa09858769b895cd1743f0bb67e02de0850237da
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-22 08:32:08 -0700

    more number list

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * more_numbers - this function print list of numb
+ * Return - return the number
+ */
+void more_numbers(void)
+{
+	for (i = 0; i < 15; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
