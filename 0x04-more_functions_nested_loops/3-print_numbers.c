be7b3ee7e1c69e68a7de4a946b071f06125e7b8e
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-22 05:37:45 -0700

    for loop that print number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - this function print number followed by line
+ * Return : return the nmbers
+ */
+void print_numbers(void)
+{
+	for (i = 0 , i < 10 , i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
