fb55ac81c49458e615ecb92f48e225e46ba7dd4e
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-26 06:26:56 -0700

    print recursivly

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include 'main.h'
+/**
+ * print_rev - this func print string in reverse
+ * @s: represent the string
+ */
+void print_rev(char *s)
+{
+	int o;
+	int stringsize = 0;
+	char empty = '\0';
+
+	while (*s != empty)
+	{
+		stringsize++;
+		s++;
+	}
+	s--;
+	for (o = stringsize; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
