a5615ec5c12d11f86055c02ef2f74c98ad2e98a1
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-29 06:24:25 -0700

    cap string fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,7 @@ char *cap_string(char *str)
 		while (!(str[x] >= 'a' && str[x] <= 'z'))
 			x++;
 			if (str[x - 1] == ' ' || str[x - 1] == '\t' || str[x - 1] == '\n' || str[x - 1] == ',' || str[x - 1] == ';' || str[x - 1] == '.' || str[x - 1] == '!' || str[x - 1] == '?' || str[x - 1] == '"' || str[x - 1] == '(' || str[x - 1] == ')' || str[x - 1] == '{' || str[x - 1] == '}' || x == 0)
-			{	str[x] -= 32;
-			}
+				str[x] -= 32;
 		x++;
 	}
 	return (str);
