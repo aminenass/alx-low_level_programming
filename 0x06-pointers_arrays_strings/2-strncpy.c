9edf3e8f642f0b3bf85f28a44a31496d2efedecf
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-29 03:32:56 -0700

    third function 2-strcat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * __strncpy - third function 
+ * @dest: first input
+ * @src: second input
+ * @n: third input
+ * Return: always success
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int first;
+
+	first = 0;
+	while (first < n && src[first] != '\0')
+	{
+		dest[first] = src[first];
+		first++;
+	}
+	while (first < n)
+	{
+		dest[first] = '\0';
+		first++;
+	}
+
+	return (dest);
+}
