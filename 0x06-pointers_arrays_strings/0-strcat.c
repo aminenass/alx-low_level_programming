3cf1fe4b45d94d27094a36202858bc7882f0e6cc
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-29 03:14:23 -0700

    0strcat conce two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * char_strcat - this function conc two string
+ * @dest: first input
+ * @src: second valude
+ */
+char *_strcat(char *dest, char *src)
+{
+	int first;
+	int second;
+
+	first = 0;
+	second = 0;
+	while (dest[first] != '\0')
+	{
+		first++;
+	}
+	while (src[second] != '\0')
+	{
+		dest[first] = src[second];
+		first++;
+		second++;
+	}
+	dest[first] = '\0';
+	return (dest);
+}
