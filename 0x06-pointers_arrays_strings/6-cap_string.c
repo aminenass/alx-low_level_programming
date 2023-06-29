f8cfdbb69268c718e99b48c6c54aebd36cbb5b22
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-29 06:32:14 -0700

    cap string fixage 1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,7 @@ char *cap_string(char *str)
 	while (str[x])
 	{
 		while (!(str[x] >= 'a' && str[x] <= 'z'))
+			{
 			x++;
 			if (str[x - 1] == ' ' ||
 				       	str[x - 1] == '\t' ||
@@ -27,6 +28,7 @@ char *cap_string(char *str)
 				       	str[x - 1] == '}' 
 					|| x == 0)			
 						str[x] -= 32;
+			}
 					x++;
 	}
 	return (str);
