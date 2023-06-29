ebd22da9d8ac6597300f9adfb5ce058fc82f2a62
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-29 06:49:34 -0700

    capitlaize fixage

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,26 +10,30 @@ char *cap_string(char *str)
 
 	while (str[x])
 	{
-		while (!(str[x] >= 'a' && str[x] <= 'z'))
-			{
-			x++;
-			if (str[x - 1] == ' ' ||
-				       	str[x - 1] == '\t' ||
-				       	str[x - 1] == '\n' ||
-				       	str[x - 1] == ',' ||
-				       	str[x - 1] == ';' ||
-				       	str[x - 1] == '.' || 
-					str[x - 1] == '!' || 
-					str[x - 1] == '?' ||
-				       	str[x - 1] == '"' ||
-				       	str[x - 1] == '(' ||
-				       	str[x - 1] == ')' ||
-				       	str[x - 1] == '{' ||
-				       	str[x - 1] == '}' 
-					|| x == 0)			
-						str[x] -= 32;
-			}
-					x++;
+	while (!(str[x] >= 'a' && str[x] <= 'z'))
+	{
+    		x++;
+    		if (str[x - 1] == ' ' ||
+		str[x - 1] == '\t' ||
+		str[x - 1] == '\n' ||
+		str[x - 1] == ',' ||
+		str[x - 1] == ';' ||
+		str[x - 1] == '.' ||
+		str[x - 1] == '!' ||
+		str[x - 1] == '?' ||
+		str[x - 1] == '"' ||
+		str[x - 1] == '(' ||
+		str[x - 1] == ')' ||
+		str[x - 1] == '{' ||
+		str[x - 1] == '}')
+    		{
+		if (str[x] >= 'a' && str[x] <= 'z')
+		{
+    			str[x] -= 32;
+		}
+    		}
+	}
+				x++;
 	}
 	return (str);
 }
