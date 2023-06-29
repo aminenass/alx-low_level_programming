036d9a6225675c9d83a2f015add9a38a8bc8bfc7
mohamed amine nassim <m.nassim152@gmail.com>
2023-06-29 06:28:00 -0700

    cap string fixage

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,22 @@ char *cap_string(char *str)
 	{
 		while (!(str[x] >= 'a' && str[x] <= 'z'))
 			x++;
-			if (str[x - 1] == ' ' || str[x - 1] == '\t' || str[x - 1] == '\n' || str[x - 1] == ',' || str[x - 1] == ';' || str[x - 1] == '.' || str[x - 1] == '!' || str[x - 1] == '?' || str[x - 1] == '"' || str[x - 1] == '(' || str[x - 1] == ')' || str[x - 1] == '{' || str[x - 1] == '}' || x == 0)
-				str[x] -= 32;
-		x++;
+			if (str[x - 1] == ' ' ||
+				       	str[x - 1] == '\t' ||
+				       	str[x - 1] == '\n' ||
+				       	str[x - 1] == ',' ||
+				       	str[x - 1] == ';' ||
+				       	str[x - 1] == '.' || 
+					str[x - 1] == '!' || 
+					str[x - 1] == '?' ||
+				       	str[x - 1] == '"' ||
+				       	str[x - 1] == '(' ||
+				       	str[x - 1] == ')' ||
+				       	str[x - 1] == '{' ||
+				       	str[x - 1] == '}' 
+					|| x == 0)			
+						str[x] -= 32;
+					x++;
 	}
 	return (str);
 }
